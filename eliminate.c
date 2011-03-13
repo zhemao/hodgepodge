08bc92fce8a682ce4f8006bf48b23255668cf18e
Zhehao Mao <zhehao.mao@gmail.com>
2011-03-13 19:11:02 -0400

    Added README and changed eliminate

--- eliminate.c
+++ eliminate.c
@@ -2,29 +2,27 @@
 #include <stdlib.h>
 #include <string.h>
 
-void eliminate(char *fname){
-	FILE * in = fopen(fname, "r");
-	char pref[100] = "min-";
-	fname = strcat(pref, fname);
-	FILE * out = fopen(fname, "w");
-	char c = fgetc(in);
-	while(c!=EOF){
-		if(!(c=='\n'||c==' '||c=='\t'))
-			fputc((int)c, out);
-		c = fgetc(in);
-	}
-	fclose(in);
-	fclose(out);
-}
-
 int main(int argc, char *argv[]){
-	int x;
-	printf("starting\n");
-	for(x=1;x<argc;x++){
-		char str[100];
-		strcpy(str, argv[x]);
-		printf("eliminating %s\n", str);
-		eliminate(str);
-	}
+  FILE *in, *out;
+  char c;
+  if(argc>1){
+    in = fopen(argv[1], "r");
+    if(argc>2){
+      out = fopen(argv[2], "w");
+    }
+    else{
+      out = stdout;
+    }
+  }
+  else{
+    in = stdin;
+    out = stdout;
+  }
+  c = fgetc(in);
+  while(c!=EOF){
+    if(!(c=='\n'||c==' '||c=='\t'))
+      fputc((int)c, out);
+    c = fgetc(in);
+  }
 }
 
