d5b1921d117232734943d8f5a474c4d9d98000e3
Zhehao Mao <zhehao.mao@gmail.com>
2011-09-04 12:13:41 -0400

    improve autosync script

--- autosync.sh
+++ autosync.sh
@@ -5,14 +5,17 @@ REMOTEDIR=$2
 
 rsync -avz --delete $LOCALDIR $REMOTEDIR
 
-EVENTS="CREATE,CLOSE_WRITE,DELETE,MOVED_FROM,MOVED_TO"
+EVENTS="CREATE,CLOSE_WRITE,DELETE"
+
+oldtime=`date +%S`
 
 inotifywait -mr -e $EVENTS $LOCALDIR | \
 	while read dir event file; do
-		date +"Received $event at %Y/%m/%d %H:%M:%S"
-		if [ $event == "DELETE" ]; then
+		time=`date +%S`
+		timediff=`echo "$time-$oldtime" | bc`
+		if [ $timediff -gt 2 ]; then
+			date +"Received $event at %Y/%m/%d %H:%M:%S"
 			rsync -avz --delete $LOCALDIR $REMOTEDIR
-		else
-			rsync -avz $LOCALDIR/$file $REMOTEDIR/$file 
 		fi
+		oldtime=time
 	done
