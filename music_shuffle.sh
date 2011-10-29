a467b4f168b7c7f1570e192e1e4b045fde57be5c
Zhehao Mao <zhehao.mao@gmail.com>
2011-10-29 14:05:36 -0400

    give option to use different music player

--- music_shuffle.sh
+++ music_shuffle.sh
@@ -11,16 +11,19 @@ else
 	MUSIC_FOLDER=$1
 fi
 
-music_player='mplayer'
+if [ -z "$MUSIC_PLAYER" ]; then
+	MUSIC_PLAYER='mplayer'
+fi
 
 if [ -d $MUSIC_FOLDER ]; then
 	music_list='/tmp/music_list.txt'
-	find $MUSIC_FOLDER -name \*.mp3 -or -name \*.ogg > $music_list
+	find $MUSIC_FOLDER -name \*.mp3 -or -name \*.ogg \
+		-or -name \*.wav -or -name \*.flac > $music_list
 	numfiles=`wc -l < $music_list`
 	while [ "$?" == "0" ]; do
 		lineno=`echo "$RANDOM % $numfiles" | bc`
 		echo $lineno
 		file="`lrange $lineno $lineno $music_list`"
-		$music_player "$file"
+		$MUSIC_PLAYER "$file"
 	done
 fi
