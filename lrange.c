cf951501ae8b9f7466b06a30e3606cd0647d4a0f
Zhehao Mao <zhehao.mao@gmail.com>
2011-10-29 13:46:13 -0400

    push all errors to stderr

--- lrange.c
+++ lrange.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]){
 	int lineno = 1;
 
 	if(argc < 3){
-		printf("Usage: %s start end [infile] [outfile]\n", argv[0]);
+		fprintf(stderr, "Usage: %s start end [infile] [outfile]\n", argv[0]);
 		exit(EXIT_FAILURE);
 	}
 
