48d02d47c923951daa1e9e2bc3d5bd217c34321b
Zhehao Mao <zhehao.mao@gmail.com>
2011-12-06 23:54:53 -0500

    fix up dbus-control

--- dbus-control.sh
+++ dbus-control.sh
@@ -1,13 +1,22 @@
-CONSOLE_KIT='dbus-send --system --print-reply \
-	--dest="org.freedesktop.ConsoleKit" /org/freedesktop/ConsoleKit/Manager \
-	org.freedesktop.ConsoleKit.Manager'
-UPOWER='dbus-send --system --print-reply \
-	--dest="org.freedesktop.UPower" /org/freedesktop/UPower \
-	org.freedesktop.UPower'
+#!/bin/sh
+
+function dbus_send_ck {
+	dbus-send --system --print-reply \
+		--dest="org.freedesktop.ConsoleKit" \
+		/org/freedesktop/ConsoleKit/Manager \
+		org.freedesktop.ConsoleKit.Manager.$1
+}
+
+function dbus_send_upower {
+	dbus-send --system --print-reply \
+		--dest="org.freedesktop.UPower" /org/freedesktop/UPower \
+		org.freedesktop.UPower.$1
+}
 
 case $1 in 
-	stop) $CONSOLE_KIT.Stop ;;
-	restart) $CONSOLE_KIT.Restart ;;
-	suspend) $UPOWER.Suspend ;;
+	stop) dbus_send_ck Stop ;;
+	restart) dbus_send_ck Restart ;;
+	suspend) dbus_send_upower Restart ;;
+	*) echo "Usage: $0 stop|restart|suspend" ;;
 esac
 
