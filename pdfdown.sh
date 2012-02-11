775c107fc18c08742e1baf5ec5d44eae3d0082d6
Zhehao Mao <zhehao.mao@gmail.com>
2012-02-11 09:38:44 -0500

    use markdown_py and xhtml2pdf in pdfdown

--- pdfdown.sh
+++ pdfdown.sh
@@ -2,12 +2,13 @@
 
 # Convert Markdown to PDF Format using markdown and htmldoc
 
-if [ -z "$2" ]; then
-	markdown | htmldoc - --format pdf14
+if [ -z "$1" ]; then
+	echo "Must supply input filename"
 else
 	if [ -z "$2" ]; then 
-		markdown $1 | htmldoc - --format pdf14
+		PDFNAME=`echo $1 | sed -e "s/\..*/.pdf/"`
+		markdown_py $1 | xhtml2pdf - $PDFNAME
 	else
-		markdown $1 | htmldoc - --format pdf14 > $2
+		markdown_py $1 | xhtml2pdf - $2
 	fi
 fi
