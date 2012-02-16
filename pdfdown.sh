950161b41d5c229c0d45dc9b5926b4254b058502
Zhehao Mao <zhehao.mao@gmail.com>
2012-02-16 18:04:18 -0500

    allow pdfdown to take extra arguments

--- pdfdown.sh
+++ pdfdown.sh
@@ -5,10 +5,6 @@
 if [ -z "$1" ]; then
 	echo "Must supply input filename"
 else
-	if [ -z "$2" ]; then 
-		PDFNAME=`echo $1 | sed -e "s/\..*/.pdf/"`
-		markdown_py $1 | xhtml2pdf - $PDFNAME
-	else
-		markdown_py $1 | xhtml2pdf - $2
-	fi
+	PDFNAME=`echo $1 | sed -e "s/\..*/.pdf/"`
+	markdown_py $@ | xhtml2pdf - $PDFNAME
 fi
