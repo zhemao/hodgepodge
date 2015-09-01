c951dc7d8f354a07853789b6edb7ca2e2434f6ff
Howard Mao <zhehao.mao@gmail.com>
2015-08-31 23:05:05 -0700

    tabs to spaces in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 EXECUTABLES="minify sanitize inserttext todec fromdec lrange timestamp \
-	echorandom xmarquee randomdig"
+    echorandom xmarquee randomdig"
 
 PYTHON=/usr/bin/python2
 
@@ -16,19 +16,19 @@ echo "Installing shell scripts"
 
 for file in *.sh
 do
-	if [ $file != "install.sh" ]; then
-		shortname=`echo $file | sed -e "s/\..*//"`
+    if [ $file != "install.sh" ]; then
+    	shortname=`echo $file | sed -e "s/\..*//"`
         cp $PWD/$file $HOME/bin/$shortname
-	fi
+    fi
 done
 
 echo "Installing python scripts"
 
 for file in *.py
 do
-	shortname=`echo $file | sed -e "s/\..*//"`
-	rm -f $HOME/bin/$shortname
-	echo "#!$PYTHON" > $HOME/bin/$shortname
-	cat $file >> $HOME/bin/$shortname
-	chmod +x $HOME/bin/$shortname
+    shortname=`echo $file | sed -e "s/\..*//"`
+    rm -f $HOME/bin/$shortname
+    echo "#!$PYTHON" > $HOME/bin/$shortname
+    cat $file >> $HOME/bin/$shortname
+    chmod +x $HOME/bin/$shortname
 done
